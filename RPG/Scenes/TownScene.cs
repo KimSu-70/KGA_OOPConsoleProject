faca678f490097de8dea5cc8ae5bf381faf4d286
KimSu-70 <rlatngh4770@naver.com>
2024-08-14 15:14:55 +0900

    feat : 마을 구현

--- RPG/Scenes/TownScene.cs
+++ RPG/Scenes/TownScene.cs
@@ -1,38 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RPG.Scenes
+﻿namespace RPG.Scenes
 {
     public class TownScene : Scene
     {
+        private string input;
         public TownScene(Game game) : base(game)
         { }
         public override void Enter()
         {
-            throw new NotImplementedException();
+            Console.Clear();
+            Console.WriteLine("마을로 이동합니다");
+            Thread.Sleep(1500);
         }
 
         public override void Exit()
         {
-            throw new NotImplementedException();
+
         }
 
         public override void Input()
         {
-            throw new NotImplementedException();
+            input = Console.ReadLine();
         }
 
         public override void Render()
         {
-            throw new NotImplementedException();
+            Console.Clear();
+            game.Player.ShowInfo();
+            Console.WriteLine("초보자 마을");
+            Console.WriteLine("무엇을 하시겠습니까?");
+            Console.WriteLine("1. 여관에서 휴식을 취한다 (체력 전부 회복/ 20G)");
+            Console.WriteLine("2. 숲으로 이동한다");
+            Console.WriteLine("3. 상태창을 확인한다.");
         }
 
         public override void Update()
         {
-            throw new NotImplementedException();
+            switch (input)
+            {
+                case "1":
+                    game.ChangeScene(SceneType.Hotel);
+                    break;
+                case "2":
+                    game.ChangeScene(SceneType.Map);
+                    break;
+                case "3":
+                    game.ChangeScene(SceneType.Stat);
+                    break;
+            }
         }
     }
 }
